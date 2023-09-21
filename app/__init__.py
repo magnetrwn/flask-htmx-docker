8c1ba57b1fe1b04dc0e0d11cecc92b0ba73f34e8
magnetrwn <rpolli.dev@tutanota.com>
2023-09-21 10:18:06 +0200

    Starts up fine now!

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .views import *
+from .models import *
 
 persistent_path = os.getenv("PERSISTENT_STORAGE_DIR", os.path.dirname(os.path.realpath(__file__)))
 
